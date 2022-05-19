d12dec75202d8b71fbf69d20ad38460d559958bc
G4MAlexVannorsdall <94846507+G4MAlexVannorsdall@users.noreply.github.com>
2022-05-19 10:47:46 +0100

    PHP-103- Edited GetShoppingListDetailRequest and created GetShoppingListDetailRequest test, which passes

--- src/Application/Queries/GetShoppingListDetail/GetShoppingListDetailRequest.php
+++ src/Application/Queries/GetShoppingListDetail/GetShoppingListDetailRequest.php
@@ -4,21 +4,42 @@ namespace Lindyhopchris\ShoppingList\Application\Queries\GetShoppingListDetail;
 
 class GetShoppingListDetailRequest
 {
-    public string $slug;
-    public int $filterValue;
+    /**
+     * @var string
+     */
+    private string $slug;
 
-    public function __construct(string $slug, int $filterValue)
+    /**
+     * @var string
+     */
+    private string $filterValue;
+
+    /**
+     * @param string $slug
+     * @param string $filterValue
+     */
+    public function __construct(string $slug, string $filterValue)
     {
         $this->slug = $slug;
         $this->filterValue = $filterValue;
     }
 
-    private function getSlug(): string
+    /**
+     * Returns the slug (title) of the list.
+     *
+     * @return string
+     */
+    public function getSlug(): string
     {
         return $this->slug;
     }
 
-    private function getFilterValue(): int
+    /**
+     * Returns whether the user requested all, complete, or not complete items of the list.
+     *
+     * @return string
+     */
+    public function getFilterValue(): string
     {
         return $this->filterValue;
     }

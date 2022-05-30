ee010a6c0447cf1e5b940e922344beeefe266eee
G4MAlexVannorsdall <94846507+G4MAlexVannorsdall@users.noreply.github.com>
2022-05-30 15:47:26 +0100

    PHP-107- edited after PR

--- tests/Unit/Application/Commands/ArchiveShoppingList/ArchiveShoppingListModelTest.php
+++ tests/Unit/Application/Commands/ArchiveShoppingList/ArchiveShoppingListModelTest.php
@@ -8,21 +8,12 @@ use PHPUnit\Framework\TestCase;
 class ArchiveShoppingListModelTest extends TestCase
 {
     /**
-     * @return string
-     */
-    public function listProvider(): string
-    {
-        return 'groceries';
-    }
-    /**
-     * @param string $list
      * @return void
-     * @dataProvider listProvider
      */
-    public function test(string $list): void
+    public function test(): void
     {
         $model = new ArchiveShoppingListModel('groceries');
 
-        $this->assertSame($list, $model->getList());
+        $this->assertSame('groceries', $model->getList());
     }
 }
